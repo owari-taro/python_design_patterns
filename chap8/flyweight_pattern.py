3d4aae1e7b925754eb6e803d4a7ee9dc453b2aef
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-21 18:08:03 +0900

    Create flyweight_pattern.py

---
+++ chap8/flyweight_pattern.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+CarType=Enum('CarType','subcompact compact suv')
+
+
+class Car:
+  pool=dict()
+  
+  def __new__(cls,car_type):
+    print(cls.pool)
+    obj=cls.pool.get(car_type,None)
+    if not obj:
+      obj=object.__new__(cls)
+      cls.pool[car_type]=obj
+      obj.car_type=car_type
+    return obj
+  
+  def render(self,color,x,y):
+    type=self.car_type
+    msg=f'render a car of type {type} and color {color} at ({x},{y})'
+    print(msg)
+    
+  
