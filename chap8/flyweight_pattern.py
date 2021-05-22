72e4d231b636c0626e2b53526749117981db9496
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-23 00:10:16 +0900

    update flyweightpattern

--- chap8/flyweight_pattern.py
+++ chap8/flyweight_pattern.py
@@ -1,23 +1,21 @@
 from enum import Enum
 
-CarType=Enum('CarType','subcompact compact suv')
+CarType = Enum('CarType', 'subcompact compact suv')
 
 
 class Car:
-  pool=dict()
-  
-  def __new__(cls,car_type):
-    print(cls.pool)
-    obj=cls.pool.get(car_type,None)
-    if not obj:
-      obj=object.__new__(cls)
-      cls.pool[car_type]=obj
-      obj.car_type=car_type
-    return obj
-  
-  def render(self,color,x,y):
-    type=self.car_type
-    msg=f'render a car of type {type} and color {color} at ({x},{y})'
-    print(msg)
-    
-  
+    pool = dict()
+
+    def __new__(cls, car_type):
+        print(cls.pool)
+        obj = cls.pool.get(car_type, None)
+        if not obj:
+            obj = object.__new__(cls)
+            cls.pool[car_type] = obj
+            obj.car_type = car_type
+        return obj
+
+    def render(self, color, x, y):
+        type = self.car_type
+        msg = f'render a car of type {type} and color {color} at ({x},{y})'
+        print(msg)
